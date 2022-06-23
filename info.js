b04a0fcef16785dc15daf8875fb711fccacee8f5
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2022-06-23 15:41:33 +0100

    Delete info.js

--- info.js
+++
@@ -1,5 +0,0 @@
-const info = ['LUGHT-THIE', 'dooinney', 'ben', 'inneen', 'mac', 'cloan', 'ENMYN', "Çhalse ta'n ennym orrym.", "Ta my ven enmyssit Aalish.", "Ta daa chloan aym. Un vac as un inneen.", "Ta my vac enmyssit Juan.", "Ta my 'neen enmyssit Jonee.", "GOOIN LHIU?", "Cre'n ennym t'er my 'neen?"];
-
-function changeInfo () {
-    document.getElementById('info-box').textContent = info[img];
-}
